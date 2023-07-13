5eb927609eb4943aa16e8b7f8c7541bcb3f81fdc
Krzysztof Wiskowski <krzysztof.wiskowski@gmail.com>
2023-07-13 22:44:21 +0200

    updated gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,15 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector("ul.gallery");
+const galleryImages = galleryItems
+  .map(
+    (image) =>
+      `<div class = "gallery__item">
+            <a class = "gallery__link" href = "${image.original}">
+                <img class = "gallery__image" data-source = "${image.original}" src = "${image.preview}" alt = "${image.description}" />
+            </a>
+        </div>`
+  )
+  .join("");
+gallery.insertAdjacentHTML("beforeend", galleryImages);
