c73a0a75c56b56e3e7adfc1d5474900cd2193cae
Krzysztof Wiskowski <krzysztof.wiskowski@gmail.com>
2023-08-15 14:23:59 +0200

    bugfix

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,15 +1,12 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-let lightbox = new SimpleLightbox(".gallery a", {
-  /* options */
-});
 const gallery = document.querySelector("ul.gallery");
 const galleryImages = galleryItems
   .map(
     (image) =>
       `<li><a class="gallery__item" href="${image.original}">
-      <img class="gallery__image" src="${image.preview}" alt="${image.description}" />
+      <img class="gallery__image" src="${image.preview}" title="${image.description}" alt="${image.description}" />
     </a></li>`
   )
   .join("");
@@ -17,19 +14,26 @@ gallery.insertAdjacentHTML("beforeend", galleryImages);
 
 gallery.addEventListener("click", (e) => {
   e.preventDefault();
-  if (e.target.nodeName !== "IMG") return;
+
+  if (e.target.nodeName !== "img") return;
   const handleEscapeKey = (e) => {
     if (e.key === "Escape") instance.close();
   };
-  const instance = basicLightbox.create(`<img src="${e.target.src.source}">`, {
-    onShow: () => {
-      document.addEventListener("keydown", handleEscapeKey);
-    },
-    onClose: () => {
-      document.removeEventListener("keydown", handleEscapeKey);
-    },
-  });
+  const instance = simpleLightbox.create(
+    `<img src="${e.target.dataset.source}">`,
+    {
+      onShow: () => {
+        document.addEventListener("keydown", handleEscapeKey);
+      },
+      onClose: () => {
+        document.removeEventListener("keydown", handleEscapeKey);
+      },
+    }
+  );
   instance.show();
 });
 
-console.log(galleryItems);
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+  overlayOpacity: 0.5,
+});
